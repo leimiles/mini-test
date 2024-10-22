ef879bdf6f821e230fac65824f5323a4e3f75556
zhulei <zhulei@xmfunny.com>
2024-10-22 17:59:32 +0800

    <feature> update scene

---
+++ Assets/Arts/BearPunch/Animations/Riggings/Scripts/StandOnTheGround.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Collider))]
+public class StandOnTheGround : MonoBehaviour
+{
+    void OnCollisionEnter()
+    {
+    }
+}
